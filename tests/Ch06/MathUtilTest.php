d5991372f9ff138f83d603521a7bb006cf0a7536
blue32a <blue32a.jp@gmail.com>
2023-02-03 22:29:45 +0900

    振る舞いへの依存を分離する
    リスト6-9, 6-10, 6-11

--- tests/Ch06/MathUtilTest.php
+++ tests/Ch06/MathUtilTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Ch06;
 
+use Chozetsu\Ch06\Math;
 use Chozetsu\Ch06\MathUtil;
 use PHPUnit\Framework\TestCase;
 
@@ -9,7 +10,7 @@ class MathUtilTest extends TestCase
 {
     public function testSaturate(): void
     {
-        $mathUtil = new MathUtil();
+        $mathUtil = new MathUtil(new Math());
 
         // 範囲内ならそのまま
         $this->assertEquals(2, $mathUtil->saturate(2, 1, 3));
@@ -21,5 +22,36 @@ class MathUtilTest extends TestCase
         // 上限値／下限値と同じ値は範囲内である
         $this->assertEquals(1, $mathUtil->saturate(1, 1, 3));
         $this->assertEquals(3, $mathUtil->saturate(3, 1, 3));
+    }
+    public function testSaturateUseStub(): void
+    {
+        $mathStub = $this->createStub(Math::class);
+        $mathUtil = new MathUtil($mathStub);
+
+        $mathStub->method('max')->willReturn(2);
+        $mathStub->method('min')->willReturn(2);
+        $result = $mathUtil->saturate(2, 1, 3);
+
+        $this->assertEquals(2, $result);
+    }
+
+    public function testSaturateUseMock(): void
+    {
+        $math = $this->createMock(Math::class);
+        $mathUtil = new MathUtil($math);
+
+        $math->expects($this->atLeastOnce())
+            ->method('max')
+            ->with($this->equalTo(2), $this->equalTo(1))
+            ->willReturn(2);
+
+        $math->expects($this->atLeastOnce())
+            ->method('min')
+            ->with($this->equalTo(2), $this->equalTo(3))
+            ->willReturn(2);
+
+        $result = $mathUtil->saturate(2, 1, 3);
+
+        $this->assertEquals(2, $result);
     }
 }
