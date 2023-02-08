bef70590c2bba2ce50b6dca63246e7da7acf0e03
blue32a <blue32a.jp@gmail.com>
2023-02-08 22:16:40 +0900

    DIコンテナを利用
    リスト 7-10 ~ 7-14

--- src/Ch07/FizzBuzz/App.php
+++ src/Ch07/FizzBuzz/App.php
@@ -2,12 +2,25 @@
 
 namespace Chozetsu\Ch07\FizzBuzz;
 
+use Chozetsu\Ch07\FizzBuzz\App\FizzBuzzSequencePrinter;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
 class App
 {
     public static function main(): void
     {
-        $factory = new FizzBuzzAppFactory();
-        $printer = $factory->create();
-        $printer->printRange(1, 100);
+        $containerBuilder = new ContainerBuilder();
+        $loader = new YamlFileLoader(
+            $containerBuilder,
+            new FileLocator(__DIR__ . '/../../../config/ch07')
+        );
+        $loader->load('services.yaml');
+        $containerBuilder->compile();
+
+        $containerBuilder
+            ->get(FizzBuzzSequencePrinter::class)
+            ->printRange(1, 100);
     }
 }
